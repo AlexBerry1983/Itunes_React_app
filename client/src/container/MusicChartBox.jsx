de94653087c6b0e323ae9bbbf7d573803f3e96a5
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-11 19:23:14 +0100

    add AJAX request

--- client/src/container/MusicChartBox.jsx
+++ client/src/container/MusicChartBox.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 import MusicChartList from '../components/MusicChartList'
 
 class MusicChartBox extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      ChartMusicData: []
+    }
+  }
+
+  componentDidMount(){
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
+    const request = new XMLHttpRequest();
+    request.open("GET", url)
+    request.onload = () => {
+      if (request.status !== 200) return
+
+      const jsonString = request.responseText
+      const MusicData = JSON.parse(jsonString)
+
+      this.setState({ChartMusicData: MusicData})
+      console.log(MusicData);
+    }
+    request.send()
+  }
+
   render(){
     return (
       <div>
