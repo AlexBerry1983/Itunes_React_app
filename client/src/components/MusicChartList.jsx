12722b6a338631b9c55aa95b02267133284b3b5e
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-11 17:39:21 +0100

    add music list component skeleton

---
+++ client/src/components/MusicChartList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MusicChartList extends React.Component {
+
+  render(){
+    return (
+      <h2>This is the music list</h2>
+    )
+  }
+}
+
+export default MusicChartList
--- client/src/container/MusicChartBox.jsx
+++ client/src/container/MusicChartBox.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import MusicChartList from '../components/MusicChartList'
 
 class MusicChartBox extends React.Component{
   render(){
     return (
-      <h1>Music list goes here</h1>
+      <div>
+        <h1>Music list goes here</h1>
+        <MusicChartList />
+      </div>
     )
   }
 }
