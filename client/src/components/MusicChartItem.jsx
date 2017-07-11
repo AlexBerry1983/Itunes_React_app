81b345d7b3a589d2fdfd1507108ee688c5d1665d
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-11 23:21:07 +0100

    styling

--- client/src/components/MusicChartItem.jsx
+++ client/src/components/MusicChartItem.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 //chart postion, title and artist
 export const MusicChartItem = (props) => {
   return (
-    <div>
-      <ul> Song details :-
+    <div className="songItem">
+      <img className="img" src={props.image}/>
+      <ul> 
         <li>Title: {props.title}</li>
         <li>Artist: {props.artist}</li>
         <li>Chart Position: {props.chartPosition}</li>
--- client/src/components/MusicChartList.jsx
+++ client/src/components/MusicChartList.jsx
@@ -5,11 +5,16 @@ class MusicChartList extends React.Component {
 
   render(){
     const musicToRender = this.props.chartMusicData.map((song, index) =>{
-      console.log(song);
-      return<MusicChartItem key={index} title={song['im:collection']['im:name'].label} artist={song['im:artist'].label} chartPosition={song.category.attributes['im:id']} />
+      console.log(song['im:image']);
+      return<MusicChartItem key={index}
+        title={song['im:collection']['im:name'].label}
+        artist={song['im:artist'].label}
+        chartPosition={song.category.attributes['im:id']}
+        image={song['im:image'][2].label}/>
     })
     return (
       <div>
+        <h1>Top 20 Chart Tunes July 2017</h1>
         {musicToRender}
       </div>
     )
